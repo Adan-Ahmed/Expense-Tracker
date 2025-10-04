675e9ce9ab8c17a72e7059f51afba6aff1423d1f
Adan  Ahmed <adan.ahmed.work@gmail.com>
2025-10-04 18:25:21 +0500

    Update ET.py

--- ET.py
+++ ET.py
@@ -4,6 +4,7 @@ import os
 # The list hold the expense in memory whule program run
 expenses = []
 
+# 1 add_expense
 def add_expense():
     print("\n -------- Add a New Expense --------")
     amount = float(input("Enter the amount of spent: Rs"))
@@ -21,7 +22,7 @@ def add_expense():
     expenses.append(new_expense)
     print("✅ Expense added successfully!")
 
-
+# 2 view_expenses
 def view_expense():
     print("\n -------- All Expense --------")
     if not expenses: #check it list is empty
@@ -29,14 +30,66 @@ def view_expense():
         return
 
     #loop through the list and print each expense
-    for i, expense in enumurate(expenses, start = 1):
+    for i, expense in enumerate(expenses, start = 1):
         print(f"{i}. Rs{expense['amount']} - {expense['description']} ({expense['category']})")
 
+# 3 view_summary
 def view_summary():
     print("\n -------- Expense Summary --------")
     if not expenses:
         print("No expense summarize")
         return
     total = sum(expense['amount'] for expense in expenses)
-    pass
+    print(f"Total Amount spent: Rs{total : 2f}")
 
+# 4 save_expenses
+def save_expenses():
+    with open('expense.txt', 'w') as file:
+        # convert file into json
+        json.dump(expenses, file)
+        print("Data saved.")
+
+# 5 load_expenses
+def load_expenses():
+
+# This tells the function to use the global 'expenses' list we created at the top
+    global expenses 
+    if os.path.exists('expenses.txt '):
+        try:
+            with open('expenses.txt', 'r') as file:
+                expenses = json.load(file)
+        except (json.JSONDecodeError, FileNotFoundError):
+            # If the file is empty or corrupted, start with a fresh list
+            expenses = []
+
+    #If the file doesn't exist, just keep the empty list
+    else:
+        expenses = []        
+
+# 6 
+def main():
+    load_expenses()
+    while True:
+        print("\n" + "=" * 40 )
+        print("Rs Personal Expense Tracker")
+        print("=" * 40)
+        print("1. Add New Expense")
+        print("2. View All Expenses")
+        print("3. View Summary")
+        print("4. Save & Exit")
+
+        choice = input("\n Enter your choice (1-4): ")
+        if choice == "1":
+            add_expense()
+        elif choice == "2":
+            view_expense()
+        elif choice == "3":
+            view_summary()
+        elif choice == "4":
+            save_expenses()
+            print("📝 Data Saved.")
+            break
+        else: 
+            print("Invalid choice! Re-Enter your choice again from number (1-4)")
+if __name__ == "__main__":
+    main()
